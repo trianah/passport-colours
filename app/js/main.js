edbe1fc4e696bb507cea616fd01fc580c8e313d1
Triana Padilla <mavb654@live.rhul.ac.uk>
2019-06-23 19:39:35 +0100

    fixed security alerts

--- app/js/main.js
+++ app/js/main.js
@@ -1,98 +1,100 @@
-// ..... CLEANING THE DATA .....
-var data = []
-const wrapper = d3.select('#passports-container')
-console.log(wrapper)
+console.log("hello");
 
-d3.csv('data/colors.csv', function(d, i) {
-	let r = toRgb(d.R)
-	let g = toRgb(d.G)
-	let b = toRgb(d.B)
+// // ..... CLEANING THE DATA .....
+// var data = [];
+// const wrapper = d3.select("#passports-container");
+// console.log(wrapper);
 
-	data.push({
-		country: d.Country,
-		r: r,
-		g: g,
-		b: b,
-		colour: getColour(r, g, b)
-	})
+// d3.csv("data/colors.csv", function(d, i) {
+//   let r = toRgb(d.R);
+//   let g = toRgb(d.G);
+//   let b = toRgb(d.B);
 
-	let item = wrapper.append('div')
+//   data.push({
+//     country: d.Country,
+//     r: r,
+//     g: g,
+//     b: b,
+//     colour: getColour(r, g, b)
+//   });
 
-	item.attr(`data-colour-${data[i].colour}`, '')
+//   let item = wrapper.append("div");
 
-	item.attr('data-passport', '')
+//   item.attr(`data-colour-${data[i].colour}`, "");
 
-	item
-		.append('svg')
-		.attr('class', 'icon')
-		.style(
-			'color',
-			'rgb(' + data[i].r + ',' + data[i].g + ',' + data[i].b + ')'
-		)
-		.append('use')
-		.attr('xlink:href', '#passport')
+//   item.attr("data-passport", "");
 
-	item.append('h2').text(data[i].country)
-})
+//   item
+//     .append("svg")
+//     .attr("class", "icon")
+//     .style(
+//       "color",
+//       "rgb(" + data[i].r + "," + data[i].g + "," + data[i].b + ")"
+//     )
+//     .append("use")
+//     .attr("xlink:href", "#passport");
 
-function toRgb(val) {
-	// convert strings to valid RGB values
-	return parseInt(parseFloat(val) * 255)
-}
+//   item.append("h2").text(data[i].country);
+// });
 
-function getColour(r, g, b) {
-	// convert strings to valid RGB values
-	if (r > g && r > b) {
-		return 'red'
-	}
+// function toRgb(val) {
+//   // convert strings to valid RGB values
+//   return parseInt(parseFloat(val) * 255);
+// }
 
-	if (g > r && g > b) {
-		return 'green'
-	}
+// function getColour(r, g, b) {
+//   // convert strings to valid RGB values
+//   if (r > g && r > b) {
+//     return "red";
+//   }
 
-	if (b > r && b > g) {
-		return 'blue'
-	}
-	return 'none'
-}
+//   if (g > r && g > b) {
+//     return "green";
+//   }
 
-console.log(data)
+//   if (b > r && b > g) {
+//     return "blue";
+//   }
+//   return "none";
+// }
 
-// ..... BINDING DATA .....
-// append each item to svg
+// console.log(data);
 
-// add rgb color as bg
-// add country name to svg
+// // ..... BINDING DATA .....
+// // append each item to svg
 
-// ..... FILTERING .....
-const filter = document.getElementById('filter')
-console.log(filter)
+// // add rgb color as bg
+// // add country name to svg
 
-const filters = document.querySelectorAll('[data-filter-option]')
+// // ..... FILTERING .....
+// const filter = document.getElementById("filter");
+// console.log(filter);
 
-filters.forEach(filter => {
-	filter.addEventListener('change', event => {
-		if (filter.checked) {
-			console.log(filter.getAttribute('value'))
-			Show(`data-colour-${filter.getAttribute('value')}`)
-		} else {
-			console.log('Disabled')
-		}
-	})
-})
+// const filters = document.querySelectorAll("[data-filter-option]");
 
-const wrap = document.getElementById('passports-container')
-var wrapperDivs = wrapper.select('[data-passport]')
+// filters.forEach(filter => {
+//   filter.addEventListener("change", event => {
+//     if (filter.checked) {
+//       console.log(filter.getAttribute("value"));
+//       Show(`data-colour-${filter.getAttribute("value")}`);
+//     } else {
+//       console.log("Disabled");
+//     }
+//   });
+// });
 
-function Show(val) {
-	// for item in items show if attr matches
-	console.log(val)
-	console.log(typeof wrapperDivs)
-	wrapperDivs.forEach(div => {
-		console.log('for each ...')
-		if (div.getAttribute(val) !== '') {
-			div.classList.toggle('hidden')
-			console.log('found: ' + val)
-		}
-	})
-}
+// const wrap = document.getElementById("passports-container");
+// var wrapperDivs = wrapper.select("[data-passport]");
+
+// function Show(val) {
+//   // for item in items show if attr matches
+//   console.log(val);
+//   console.log(typeof wrapperDivs);
+//   wrapperDivs.forEach(div => {
+//     console.log("for each ...");
+//     if (div.getAttribute(val) !== "") {
+//       div.classList.toggle("hidden");
+//       console.log("found: " + val);
+//     }
+//   });
+// }
